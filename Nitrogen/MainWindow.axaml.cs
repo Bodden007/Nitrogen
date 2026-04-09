66d63bcb857b263e9e8ad81c393358abc800fd9c
Bodden007 <denis-bodrov@mail.ru>
2026-04-09 17:51:52 +0500

    Added menu Pressure

--- Nitrogen/MainWindow.axaml.cs
+++ Nitrogen/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Media;
 
 namespace Nitrogen;
@@ -11,10 +12,29 @@ public partial class MainWindow : Window
 
         SystemDecorations = SystemDecorations.None;
         WindowState = WindowState.FullScreen;
+
+        this.KeyDown += MainWindow_KeyDown;
+    }
+
+    private void MainWindow_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.F1: new Nitrogen.Pressure().Show(); break;
+
+            case Key.F6: new Nitrogen.Menu().Show(); break;
+        }
     }
 
     private void Pressure_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        Pressure.Background = Brushes.Red;
+        Nitrogen.Pressure pressure = new Nitrogen.Pressure();
+        pressure.Show();
+    }
+
+    private void Menu_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Nitrogen.Menu menu = new Nitrogen.Menu();
+        menu.Show();
     }
 }
---
+++ Nitrogen/Views/Menu/Menu.axaml.cs
@@ -0,0 +1,57 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Input;
+using System;
+
+namespace Nitrogen;
+
+public partial class Menu : Window
+{
+    public Menu()
+    {
+        InitializeComponent();
+
+        SystemDecorations = SystemDecorations.None;
+        WindowState = WindowState.FullScreen;
+
+        this.KeyDown += Menu_KeyDown;
+
+        Closed += Menu_Closed;
+    }
+    private void Menu_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.Escape: Close(); break;
+
+            case Key.F1: new Nitrogen.Pressure().Show(); break;
+
+            case Key.F7: Environment.Exit(0); break;
+        }
+    }
+    private void Pressure_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Nitrogen.Pressure pressure = new Nitrogen.Pressure();
+        pressure.Show();
+    }
+
+    private void CloseApp_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        //TODO Разобраться с корректным закрытием приложения
+        //Avalonia.Application.Current?.Shutdown();
+        Environment.Exit(0);
+    }
+
+    private void Exit_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void Menu_Closed(object? sender, System.EventArgs e)
+    {
+        this.KeyDown -= Menu_KeyDown;
+        Closed -= Menu_Closed;
+    }
+
+}
