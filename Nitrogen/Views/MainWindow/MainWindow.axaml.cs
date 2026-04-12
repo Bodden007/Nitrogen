f7a3b6fbaced2d5bee6e390994b4ae47ae547d6e
Bodden007 <denis-bodrov@mail.ru>
2026-04-12 19:19:35 +0500

    Added temperature

--- Nitrogen/Views/MainWindow/MainWindow.axaml.cs
+++ Nitrogen/Views/MainWindow/MainWindow.axaml.cs
@@ -23,11 +23,17 @@ public partial class MainWindow : Window
         {
             case Key.F1: new Nitrogen.Pressure().Show(); break;
 
-            case Key.F5:new Nitrogen.Volume().Show(); break;
+            case Key.F2: new Nitrogen.Temperature().Show(); break;
+
+            case Key.F5: new Nitrogen.Volume().Show(); break;
 
             case Key.F6: new Nitrogen.Menu().Show(); break;
 
-            case Key.D2: HandlerVolumeLineDisplay(); break;
+            case Key.D1: HandlerRateLineDisplay(); break;
+
+            case Key.D2: HandlerTempLineDisplay(); break;
+
+            case Key.D3: HandlerVolumeLineDisplay(); break;
         }
     }
 
@@ -46,6 +52,12 @@ public partial class MainWindow : Window
     {
         Nitrogen.Menu menu = new Nitrogen.Menu();
         menu.Show();
+    }
+
+    private void Temp_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Nitrogen.Temperature temp = new Nitrogen.Temperature();
+        temp.Show();
     }
 
     private void HandlerVolumeLineDisplay()
@@ -69,4 +81,56 @@ public partial class MainWindow : Window
             CombiHeader.Text = "Σ Объем Цемента";
         }
     }
+
+    private void HandlerTempLineDisplay()
+    {
+        if (!TempOut.IsVisible)
+        {
+            HeaderTemp.Text = "Температура";
+
+            TempOut.IsVisible = true;
+            TempExc.IsVisible = true;
+            ValueTempExc.IsVisible = true;
+
+            ValueTempOut.Classes.Remove("StyleTextBlock_2");
+            ValueTempOut.Classes.Add("StyleTextBlock_1");
+        }
+        else
+        {
+            HeaderTemp.Text = "Температура Выход";
+
+            TempOut.IsVisible = false;
+            TempExc.IsVisible = false;
+            ValueTempExc.IsVisible = false;
+
+            ValueTempOut.Classes.Remove("StyleTextBlock_1");
+            ValueTempOut.Classes.Add("StyleTextBlock_2");
+        }
+    }
+
+    private void HandlerRateLineDisplay()
+    {
+        if (!HeaderRate_A.IsVisible)
+        {
+            HeaderRate_A.IsVisible = true;
+            ValueRate_A.IsVisible = true;
+            ValueRate.IsVisible = false;
+            HeaderRate_B.IsVisible = true;
+            ValueRate_B.IsVisible = true;
+
+            HeaderRate.Text = "Расход";
+        }
+        else
+        {
+            HeaderRate_A.IsVisible = false;
+            ValueRate_A.IsVisible = false;
+            ValueRate.IsVisible = true;
+            HeaderRate_B.IsVisible = false;
+            ValueRate_B.IsVisible = false;
+
+            HeaderRate.Text = "Σ Расход";
+        }
+    }
+
+
 }
---
+++ Nitrogen/Views/Menu/Temperature/Temperature.axaml.cs
@@ -0,0 +1,41 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Input;
+
+namespace Nitrogen;
+
+public partial class Temperature : Window
+{
+    public Temperature()
+    {
+        InitializeComponent();
+
+        WindowState = WindowState.FullScreen;
+
+        this.KeyDown += Temperature_KeyDown;
+
+        Closed += Temperature_Closed;
+    }
+
+
+
+    private void Temperature_KeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.Escape: Close(); break;
+        }
+    }
+
+    private void Exit_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void Temperature_Closed(object? sender, System.EventArgs e)
+    {
+        this.KeyDown -= Temperature_KeyDown;
+        Closed -= Temperature_Closed;
+    }
+}
